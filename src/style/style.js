48ef9cda099b44d001affd88d23964c7b3f7bd7d
Jacoblynch99 <jacoblynch99@gmail.com>
2020-09-30 11:27:13 -0500

    done

---
+++ src/style/style.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    carName: {
+        fontSize: 25,
+        fontWeight: 'bolder',
+        marginBottom: 15,
+    },
+    carPaper: {
+        marginTop: 40,
+        paddingTop: 50,
+        paddingBottom: 50,
+    },
+})
+
+export default useStyles
